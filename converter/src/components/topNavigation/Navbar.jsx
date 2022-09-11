7ff1b7992efc2532d865fb8bb0f6563e4090c664
DavidLiubchenko <sharingan18091997@gmail.com>
2022-09-11 16:39:03 +0300

    some clean

--- converter/src/components/topNavigation/Navbar.jsx
+++ converter/src/components/topNavigation/Navbar.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import S from '../../styles/navbar.module.scss'
-import {useDispatch, useSelector} from "react-redux";
 import Pages from "./navPages/Pages";
 import CurrentPairs from "./CurrentPairs";
 
-
 export const Navbar = () => {
-
-    // const currentlyCurrency = useSelector()
-    const dispatch = useDispatch()
-
     return (
         <nav className={S.nav}>
             <CurrentPairs/>
-
-
             <Pages/>
         </nav>
     );
